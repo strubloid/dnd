686b50e03c0a2bb3aeffdc1d352ad39b7198d5e9
strubloid <joserafaelmb@gmail.com>
2024-05-28 19:52:41 +0100

    fixing the library missing, now it is working and we can see both results from the youtube channel page

--- dnd-tools.js
+++ dnd-tools.js
@@ -1,5 +1,9 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
+
 require('dotenv').config();
+const EXTENSION_PATH_LIVE_CHAT_OVERLAY = path.join('extensions', 'live-chat-overlay-main');
+
 
 (async () => {
 
@@ -9,12 +13,14 @@ require('dotenv').config();
   // Launch the browser with the specified window size
   const browser = await puppeteer.launch({
     headless: false,
+    // devtools: true,
     // executablePath: '/usr/bin/google-chrome',
     // userDataDir: '/home/zero/.config/google-chrome/Default',
     args: [
       `--window-size=${width},${height}`, 
       '--disable-blink-features=AutomationControlled',
-      '--load-extension=/media/games/dev/dnd/extensions/live-chat-overlay-main'
+      `--disable-extensions-except=${EXTENSION_PATH_LIVE_CHAT_OVERLAY}`,
+      `--load-extension=${EXTENSION_PATH_LIVE_CHAT_OVERLAY}`
     ]
   });
 
@@ -25,6 +31,12 @@ require('dotenv').config();
     height: height,
   });
 
+  // await page.goto('chrome://extensions/', { waitUntil: 'networkidle2' });
+  
+  // await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 10)); 
+
+
+  
   console.log("GOOGLE Login");
   console.log("1 - Login Google Page");
   await page.goto('https://accounts.google.com', { waitUntil: 'networkidle2' });
@@ -63,14 +75,24 @@ require('dotenv').config();
   await iframe.click('tp-yt-paper-listbox ytd-menu-service-item-renderer:nth-of-type(2)');
   await new Promise(resolve => setTimeout(resolve, 1000 * 3)); 
 
-  console.log("4 - New Page URL");
   const pages = await browser.pages();
   const newPage = pages[pages.length - 1];
   if (newPage) {
       console.log('LiveChat:', newPage.url());
   }
 
-  // await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 10)); 
+  // click on pop-out-button to show the url
+  // console.log("5 - Click to get the popout");
+  await newPage.click('#pop-out-button');
+  await new Promise(resolve => setTimeout(resolve, 1000 * 2)); 
+
+  const popOutUrl = await newPage.$eval('#pop-out-url', el => el.value);
+
+  // const popoutValue = await newPage.$('#pop-out-url').value
+  console.log('Popout Value:', popOutUrl);
+
+
+  await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 10)); 
 
   await browser.close();
 })();
