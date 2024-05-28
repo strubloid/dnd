79e1afbec8077931311589dbf546a8a7f4bff9ad
strubloid <joserafaelmb@gmail.com>
2024-05-28 19:20:36 +0100

    pushing the first goal done

--- dnd-tools.js
+++ dnd-tools.js
@@ -33,72 +33,43 @@ require('dotenv').config();
   
   // Fill in the username and password
   console.log("3 - Email");
-  await new Promise(resolve => setTimeout(resolve, 1000 * 2)); 
+  await new Promise(resolve => setTimeout(resolve, 1000 * 1)); 
   await page.type('input[type="email"]', process.env.GOOGLE_USERNAME);
   await page.click('div[id="identifierNext"]');
   
   // wait for a bit
   console.log("3 - Password");
-  await new Promise(resolve => setTimeout(resolve, 1000 * 2)); 
+  await new Promise(resolve => setTimeout(resolve, 1000 * 1)); 
   await page.type('input[type="password"]', process.env.GOOGLE_PASSWORD);
   await page.click('div[id="passwordNext"]');
 
   // Wait for login to complete
   await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-  // console.log("Adding: Live Chat Overlay Extension");
-  // console.log("1 - access chrome store page");
-  // await page.goto('https://chromewebstore.google.com/detail/live-chat-overlay/aplaefbnohemkmngdogmbkpompjlijia', { waitUntil: 'networkidle2' });
-  // await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 10)); 
-
-
-
   console.log("DND PAGE on Youtube");
-
   
   console.log("1 - Accessing Page");
   await page.goto('https://www.youtube.com/@DnDBrasilCork/live', { waitUntil: 'networkidle2' });
 
-  // console.log("2 - Accepting the YouTube Cookies");
-  // await page.waitForSelector('button[aria-label="Accept all"]', { visible: true });
-  // await page.click('button[aria-label="Accept all"]');
-
   console.log("2 - Waiting for the chat iframe to load");
   await page.waitForSelector('iframe#chatframe', { visible: true, timeout: 60000 });
 
+  console.log("3 - Clicking on Iframe element to expand options (...)");
   const iframeElement = await page.$('#chat-container iframe');
   const iframe = await iframeElement.contentFrame();
   await iframe.click('yt-live-chat-app yt-live-chat-button');
 
-  console.log("3 - Click PopOut");
-  // document.querySelector('tp-yt-paper-listbox ytd-menu-service-item-renderer:nth-of-type(2)')
+  console.log("4 - Select PopOut");
   await iframe.click('tp-yt-paper-listbox ytd-menu-service-item-renderer:nth-of-type(2)');
-
-  await new Promise(resolve => setTimeout(resolve, 1000 * 5)); 
+  await new Promise(resolve => setTimeout(resolve, 1000 * 3)); 
 
   console.log("4 - New Page URL");
   const pages = await browser.pages();
-  
-  
-  // await pages[1].evaluate(() => {
-  //   console.log(document.location.href)
-  // });
-
-//   let pageIndex = 0;
-//   while (pageIndex < pages.length) {
-//     const page = pages[pageIndex];
-//     const url = await page.url();
-//     console.log(url);
-//     pageIndex++;
-// }
-
-  
   const newPage = pages[pages.length - 1];
   if (newPage) {
-      console.log('New page URL:', newPage.url());
+      console.log('LiveChat:', newPage.url());
   }
- 
-  console.log('Waiting');
+
   // await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 10)); 
 
   await browser.close();
